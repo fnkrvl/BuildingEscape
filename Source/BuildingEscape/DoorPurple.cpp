aae555773f75088d6565b045efa80cdcdbdcf7e4
Palms <franco.rvl91@gmail.com>
2021-08-10 21:29:05 -0300

    Seting up Purple Door

---
+++ Source/BuildingEscape/DoorPurple.cpp
@@ -0,0 +1,120 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DoorPurple.h"
+#include "Components/AudioComponent.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UDoorPurple::UDoorPurple()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorPurple::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialYaw = GetOwner()->GetActorRotation().Yaw;
+	CurrentYaw = InitialYaw;
+	OpenAngle += InitialYaw;
+
+	FindPressurePlate();
+	FindAudioComponent();
+}
+
+
+// Called every frame
+void UDoorPurple::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	// UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+	
+	if (TotalMassOfActor() > MassToOpenDoors)
+	{
+		OpenDoor(DeltaTime);
+
+		DoorLastOpened = GetWorld()->GetTimeSeconds();
+	}
+	else if (GetWorld()->GetTimeSeconds() - DoorLastOpened > DoorCloseDelay)
+		CloseDoor(DeltaTime);
+
+}
+
+void UDoorPurple::OpenDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::Lerp(CurrentYaw, OpenAngle, DeltaTime * DoorOpenSpeed);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+	CloseDoorSound = false;
+	if (!Audio_DoorPurple) return;
+	if (!OpenDoorSound)
+	{
+		Audio_DoorPurple->Play();
+		OpenDoorSound = true;
+	}
+}
+
+void UDoorPurple::CloseDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * DoorCloseSpeed);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+	OpenDoorSound = false;
+	if (!Audio_DoorPurple) return;;
+	if (!CloseDoorSound)
+	{
+		//AudioComponent->VolumeMultiplier(0.5f);
+		Audio_DoorPurple->Play();
+		CloseDoorSound = true;
+	}
+}
+
+float UDoorPurple::TotalMassOfActor() const
+{
+	float TotalMass = 0.f;
+
+	// Find all overlapping actors 
+	TArray<AActor*> OverlappingActors;
+	if (!Trigger_Purple)
+		return TotalMass;
+
+	if (Trigger_Purple)
+		Trigger_Purple->GetOverlappingActors(OUT OverlappingActors);
+
+	// Add up their masses
+	for (AActor* Actor : OverlappingActors)
+	{
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+	}
+	
+	return  TotalMass;
+}
+
+void UDoorPurple::FindPressurePlate()
+{
+	if (!Trigger_Purple)
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door component, but no pressure plate set."), *GetOwner()->GetName());
+}
+
+void UDoorPurple::FindAudioComponent()
+{
+	Audio_DoorPurple = GetOwner()->FindComponentByClass<UAudioComponent>();
+	if (!Audio_DoorPurple)
+		UE_LOG(LogTemp, Error, TEXT("%s missing audio component"), *GetOwner()->GetName());
+}
+
---
+++ Source/BuildingEscape/DoorPurple.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "DoorPurple.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UDoorPurple : public UActorComponent
+{
+	GENERATED_BODY()
+
+	public:	
+	// Sets default values for this component's properties
+	UDoorPurple(); 
+
+	protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenDoor(float DeltaTime);
+	void CloseDoor(float DeltaTime);
+	float TotalMassOfActor() const;
+	void FindAudioComponent();
+	void FindPressurePlate();
+
+	bool OpenDoorSound = false;
+	bool CloseDoorSound = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Door)
+	float OpenAngle = 90.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Door)
+	float DoorCloseDelay = .5f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Door)
+	float DoorOpenSpeed = .8f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Door)
+	float DoorCloseSpeed = 2.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Door)
+	float MassToOpenDoors = 80.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Door)
+	ATriggerVolume* Trigger_Purple = nullptr;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category= Door)
+	UAudioComponent* Audio_DoorPurple = nullptr;
+	
+	private:
+	float InitialYaw;
+	float CurrentYaw;
+
+	float DoorLastOpened = 0.f;
+
+};
