629d85b86c8f817e3414d273866b80f814a21854
Palms <franco.rvl91@gmail.com>
2021-08-10 21:28:15 -0300

    Seting up Blue Door

---
+++ Source/BuildingEscape/DoorBlue.cpp
@@ -0,0 +1,119 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DoorBlue.h"
+#include "Components/AudioComponent.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UDoorBlue::UDoorBlue()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorBlue::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InitialYaw = GetOwner()->GetActorRotation().Yaw;
+	CurrentYaw = InitialYaw;
+	OpenAngle += InitialYaw;
+
+	FindPressurePlate();
+	FindAudioComponent();
+}
+
+
+// Called every frame
+void UDoorBlue::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	// UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+	
+	if (TotalMassOfActor() > MassToOpenDoors)
+	{
+		OpenDoor(DeltaTime);
+
+		DoorLastOpened = GetWorld()->GetTimeSeconds();
+	}
+	else if (GetWorld()->GetTimeSeconds() - DoorLastOpened > DoorCloseDelay)
+		CloseDoor(DeltaTime);
+
+}
+
+void UDoorBlue::OpenDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::Lerp(CurrentYaw, OpenAngle, DeltaTime * DoorOpenSpeed);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+	CloseDoorSound = false;
+	if (!Audio_DoorBlue) return;
+	if (!OpenDoorSound)
+	{
+		Audio_DoorBlue->Play();
+		OpenDoorSound = true;
+	}
+}
+
+void UDoorBlue::CloseDoor(float DeltaTime)
+{
+	CurrentYaw = FMath::Lerp(CurrentYaw, InitialYaw, DeltaTime * DoorCloseSpeed);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+	GetOwner()->SetActorRotation(DoorRotation);
+
+	OpenDoorSound = false;
+	if (!Audio_DoorBlue) return;;
+	if (!CloseDoorSound)
+	{
+		//AudioComponent->VolumeMultiplier(0.5f);
+		Audio_DoorBlue->Play();
+		CloseDoorSound = true;
+	}
+}
+
+float UDoorBlue::TotalMassOfActor() const
+{
+	float TotalMass = 0.f;
+
+	// Find all overlapping actors 
+	TArray<AActor*> OverlappingActors;
+	if (!Trigger_Blue)
+		return TotalMass;
+
+	if (Trigger_Blue)
+		Trigger_Blue->GetOverlappingActors(OUT OverlappingActors);
+
+	// Add up their masses
+	for (AActor* Actor : OverlappingActors)
+	{
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+	}
+	
+	return  TotalMass;
+}
+
+void UDoorBlue::FindPressurePlate()
+{
+	if (!Trigger_Blue)
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door component, but no pressure plate set."), *GetOwner()->GetName());
+}
+
+void UDoorBlue::FindAudioComponent()
+{
+	Audio_DoorBlue = GetOwner()->FindComponentByClass<UAudioComponent>();
+	if (!Audio_DoorBlue)
+		UE_LOG(LogTemp, Error, TEXT("%s missing audio component"), *GetOwner()->GetName());
+}
